181b134a8e7a5d9479ef8c96c448efb3661f6871
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2022-05-21 18:16:10 -0400

    first commit setting up the project with default project AND default ui library configured

---
+++ projects/ui-lib/src/lib/ui-lib.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { UiLibComponent } from './ui-lib.component';
+
+
+
+@NgModule({
+  declarations: [
+    UiLibComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    UiLibComponent
+  ]
+})
+export class UiLibModule { }
